4e9c7c9373877afc73fbc5755ad08328518442c4
chaitanya singu <chaitanyasingu22@gmail.com>
2019-08-04 13:54:04 +0200

    jwt web api asp.net core

---
+++ JwtWebApi/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public ActionResult GetToken()
+        {
+            string SecretKey = "This_is_our_secret_key_for_creating_SymetricKey_encryption";
+            var SymeticSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            var SigningCredentials = new SigningCredentials(SymeticSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var Token = new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "developers",
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: SigningCredentials
+                );
+            return Ok(new JwtSecurityTokenHandler().WriteToken(Token));
+        }
+    }
+}
---
+++ JwtWebApi/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            string SecretKey = "This_is_our_secret_key_for_creating_SymetricKey_encryption";
+            var SymeticSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                   {
+                       //what to validate
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateIssuerSigningKey = true,
+                       //validate above with the below values
+                       ValidIssuer = "localhost",
+                       ValidAudience = "developers",
+                       IssuerSigningKey = SymeticSecurityKey
+                   };
+               });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
